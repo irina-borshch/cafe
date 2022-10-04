56846cd2d8a38ed5b27d188a24f5f0d54dad5ea4
irina-borshch <irina.borshchik@gmail.com>
2022-10-04 17:51:04 +0300

    add runner Main.java

--- src/main/java/com/solvd/cafe/Main.java
+++ src/main/java/com/solvd/cafe/Main.java
@@ -1,7 +1,9 @@
 package com.solvd.cafe;
 
+import com.solvd.cafe.services.MainCafe;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MainCafe.begin();
     }
 }
